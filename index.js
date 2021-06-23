5ae1dfc0ff6b6cb081b00d852bc89007b6d8b98d
ersati <piotrp92@gmail.com>
2021-06-23 01:57:33 +0100

    create variables with elements to manipulate and create function to check the buttons

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 // Import stylesheets
 import './style.css';
 
+const circles = document.querySelectorAll('.circle');
+const bars = document.querySelectorAll('.bar');
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+
+let progressNumber = 4;
+
+function checkButtons() {
+  if (progressNumber === 0) {
+    leftBtn.disabled = true;
+    console.log(leftBtn);
+  } else {
+    leftBtn.disabled = false;
+  }
+
+  if (progressNumber >= circles.length) {
+    rightBtn.disabled = true;
+    console.log(rightBtn);
+  } else {
+    rightBtn.disabled = false;
+  }
+}
+
+function updateProgress() {
+  checkButtons();
+}
+
+updateProgress();
