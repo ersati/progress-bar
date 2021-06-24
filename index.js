f7f933164ff5b46d98723cb13ab15ba2e383f0f7
ersati <piotrp92@gmail.com>
2021-06-24 01:57:14 +0100

    add to event listener, create function who update progress, change background color of circles

--- index.js
+++ index.js
@@ -5,27 +5,53 @@ const circles = document.querySelectorAll('.circle');
 const bars = document.querySelectorAll('.bar');
 const leftBtn = document.querySelector('.left-btn');
 const rightBtn = document.querySelector('.right-btn');
-
-let progressNumber = 4;
-
+const num = document.querySelector('.number');
+let progressNumber = 2;
+console.log(bars, circles);
 function checkButtons() {
+  rightBtn.disabled = false;
+  leftBtn.disabled = false;
+  if (progressNumber === circles.length) {
+    rightBtn.disabled = true;
+    console.log(rightBtn);
+  }
+
   if (progressNumber === 0) {
     leftBtn.disabled = true;
     console.log(leftBtn);
-  } else {
-    leftBtn.disabled = false;
-  }
-
-  if (progressNumber >= circles.length) {
-    rightBtn.disabled = true;
-    console.log(rightBtn);
-  } else {
-    rightBtn.disabled = false;
   }
 }
 
 function updateProgress() {
   checkButtons();
+  num.textContent = progressNumber;
+  console.log(circles[progressNumber]);
 }
 
-updateProgress();
+// updateProgress();
+
+leftBtn.addEventListener('click', () => {
+  if (progressNumber <= 0) {
+    console.log(progressNumber);
+    checkButtons();
+
+    return;
+  }
+
+  progressNumber--;
+  circles[progressNumber].classList.remove('bg-green');
+  circles[progressNumber].classList.add('bg-white');
+  num.textContent = progressNumber;
+});
+
+rightBtn.addEventListener('click', () => {
+  if (progressNumber >= circles.length) {
+    console.log(progressNumber);
+    checkButtons();
+    return;
+  }
+  updateProgress();
+  progressNumber++;
+  circles[progressNumber - 1].classList.add('bg-green');
+  num.textContent = progressNumber;
+});
